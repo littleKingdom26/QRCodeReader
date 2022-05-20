528ea47dd316a95d88e36c9c541a3676e99966de
littlekingdom <wingindream@gmail.com>
2022-05-21 03:01:39 +0900

    기본커i밋

---
+++ app/src/main/java/com/example/qrcodereader/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.qrcodereader
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.qrcodereader.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.ListenableFuture
+
+class MainActivity : AppCompatActivity() {
+
+    // 바인딩 생성 변수
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+
+    private val PERMISSIONS_REQUEST_CODE = 1
+    private val PERMISSIONS_REQUIRED = arrayOf(Manifest.permission.CAMERA)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 뷰 바인딩 설정
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (!hasPermissions(this)) {
+            requestPermissions(PERMISSIONS_REQUIRED,PERMISSIONS_REQUEST_CODE)
+        }else{
+            startCamera()
+        }
+    }
+
+    fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(context,it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == PERMISSIONS_REQUEST_CODE){
+            if(PackageManager.PERMISSION_GRANTED == grantResults.firstOrNull()){
+                Toast.makeText(this@MainActivity,"권한 요청이 승인되었습니다.",Toast.LENGTH_LONG).show()
+                startCamera()
+            }else{
+                Toast.makeText(this@MainActivity,"권한 요청이 거부되었습니다.",Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+    }
+
+    // 미리보기와 이미지 분석 시작
+    fun startCamera(){
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = getPreview()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            cameraProvider.bindToLifecycle(this,cameraSelector,preview)
+        },ContextCompat.getMainExecutor(this))
+
+    }
+
+    // 미리보기 객체 반환
+    fun getPreview(): Preview{
+        val preview : Preview = Preview.Builder().build()
+        preview.setSurfaceProvider(binding.barcodePreview.surfaceProvider)
+        return preview
+    }
+}
